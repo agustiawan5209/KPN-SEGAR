15e1545c4bcd1da478594b40a6c1a56ed0ca75b2
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-12-12 10:01:37 +0800

    Update Models Detail Pembelian Dan Pembelian

--- app/Models/DetailPembelian.php
+++ app/Models/DetailPembelian.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class DetailPembelian extends Model
 {
     use HasFactory;
+    protected $table = 'detail_pembelians';
+    protected $fillable = ['pembelian_id','nama_barang','harga','jumlah','total','potongan'];
+
+    public function pembelian(){
+        return $this->belongsTo(Pembelian::class, 'id');
+    }
 }
--- app/Models/Pembelian.php
+++ app/Models/Pembelian.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Pembelian extends Model
 {
     use HasFactory;
+
+    protected $table = 'pembelians';
+    protected $fillable = ['user_id', 'nama','email','alamat','no_hp','bank', 'tgl_transaksi','sub_total','bukti'];
+
+    public function detailPembelian(){
+        return $this->hasMany(DetailPembelian::class, 'pembelian_id','id');
+    }
 }
